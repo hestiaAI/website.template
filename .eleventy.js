a3e316a62aa3e24fa21e6f70c8729d1d599fec89
andreaskundig <andreas.kuendig@gmx.ch>
2021-03-23 13:41:15 +0100

    put _redirects in the right place #4

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy("./src/static/css");
+  eleventyConfig.addPassthroughCopy("./src/_redirects");
 
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml"
