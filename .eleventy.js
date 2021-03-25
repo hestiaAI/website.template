a5d19ae1b33393bb99c053afd7faade1b30b0efe
andreaskundig <andreas.kuendig@gmx.ch>
2021-03-25 17:23:15 +0100

    remove luxon and htmlmin #5

--- .eleventy.js
+++ .eleventy.js
@@ -1,55 +1,10 @@
-const { DateTime } = require("luxon");
-const htmlmin = require("html-minifier");
-
-
-
 module.exports = function (eleventyConfig) {
-  // Disable automatic use of your .gitignore
-  eleventyConfig.setUseGitIgnore(false);
-
-  // Merge data instead of overriding
-  eleventyConfig.setDataDeepMerge(true);
-
-  // human readable date
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
-    );
-  });
-
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy("./src/static/css");
   eleventyConfig.addPassthroughCopy("./src/_redirects");
   eleventyConfig.addPassthroughCopy("./src/static/img");
-
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml"
-  });
-
-  // date filter (localized)
-  // https://www.webstoemp.com/blog/multilingual-sites-eleventy/
-  eleventyConfig.addNunjucksFilter("date", function (date, format, locale) {
-    locale = locale ? locale : "en";
-    moment.locale(locale);
-    return moment(date).format(format);
-  });
-
-  // Copy Image Folder to /_site
-  // eleventyConfig.addPassthroughCopy("./src/static/img");
-
-  // Minify HTML
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if (outputPath && outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-
-    return content;
   });
 
   eleventyConfig.addCollection("locales", function(collectionApi) {
