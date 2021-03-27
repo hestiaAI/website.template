d090a5a0a3fdb5b29f9ae1420aa3b8de330ee42b
Olivier Lange <olange@petit-atelier.ch>
2021-03-27 16:13:13 +0100

    Replaces Snowpack run-script with NPM dev/build scripts (#7)
    To run Eleventy before Snowpack builds, we replaced the
    @snowpack/plugin-run-script that was here with NPM run-s/run-p
    scripts in package.json, because the @snowpack/plugin-run-script
    did not start Eleventy in build mode, it worked only in dev mode.

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,26 +1,28 @@
-// Snowpack configuration file
+// Snowpack configuration, integrated with Eleventy and Netlify
 // https://www.snowpack.dev/reference/configuration
 /* eslint-env node */
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    'build/11ty': { url: '/', static: true },
+    // NOTE: in dev mode, only `build/11ty` would exist
+    // (`build/snowpack` folder appears at build only)
+    'build/11ty': { url: '/', static: true },   // linked to Eleventy `dir.output` setting
   },
   plugins: [
-    [ '@snowpack/plugin-run-script', {
-        cmd: 'eleventy',    // production build command
-        watch: '$1 --watch' // (optional) dev server command
-      } ]
+    // NOTE: to run Eleventy before Snowpack builds, we replaced the
+    // @snowpack/plugin-run-script that was here with NPM run-s/run-p
+    // scripts in package.json, because the @snowpack/plugin-run-script
+    // did not start Eleventy in build mode, it worked only in dev mode.
   ],
   packageOptions: {},
   devOptions: {
-    open: 'none',
+    openUrl: 'en/',
     port: 8080,             // (default value)
     hmr: true               // (default value)
   },
   buildOptions: {
-    out: 'build/snowpack',
+    out: 'build/snowpack',  // linked to Netlify `build.publish` setting
     clean: true,            // (default value)
     metaUrlPath: 'modules',
     sourcemap: true
