99057d07adf6bc05c9e25d657d96ac01c2562c83
Olivier Lange <olange@petit-atelier.ch>
2021-05-30 22:17:02 +0200

    Adds splash page and basic theme assets

---
+++ src/components/README.md
@@ -0,0 +1,3 @@
+# Web Components
+
+Web Components of our website.
---
+++ src/site/_data/eleventyComputed.js
@@ -0,0 +1,62 @@
+const getLocales = (data) =>
+      data.collections.locales[data.page.inputPath];
+
+const findLocale = (data) =>
+      getLocales(data) && getLocales(data).locale;
+
+const findTranslations = (data) =>
+      getLocales(data) && getLocales(data).translations;
+
+const findDefaultTranslation = (data) =>
+      getLocales(data) && getLocales(data).defaultTranslation;
+
+const findPostAttributes = (data, attributeName) => {
+    // this is only relevant for posts
+    if(!data.tags || !data.tags.includes('post')){ return undefined; }
+
+    // Concatenate tags of all translations of this post.
+    // in case netlifyCMS only stores the attribute on the default translation.
+    // At least some versions of the cms do this.
+    const locales = getLocales(data);
+    const own_post_attr = data[attributeName] || [];
+    if(!locales){return own_post_attr;}
+    const attrsOfTranslated =
+          locales.translations
+                 .filter(t => t && t.page.data[attributeName])
+                 .map(t => t.page.data[attributeName]);
+    const attrsOfAllTranslations = attrsOfTranslated.reduce(
+        (concats, tags) => concats.concat(tags), own_post_attr);
+    const tagSet = new Set(attrsOfAllTranslations);
+    return [...tagSet];
+};
+
+const findId = (data) => {
+  if(data.id){ return data.id; }
+  const defaultTranslation = findDefaultTranslation(data);
+  if(defaultTranslation){
+    return defaultTranslation.page.data.id;
+  }
+  return data.id;
+};
+
+const omitSomeEleventySuppliedData = data => {
+  const {pkg, collections, eleventyComputed, ...cosher} = data;
+  return cosher;
+}
+
+
+// https://www.11ty.dev/docs/data-computed/
+// It is important to note that Computed Data is computed
+// right before templates are rendered. Therefore Computed Data
+// cannot be used to modify the special data properties used
+// to configure templates (e.g. layout, pagination, tags etc.).
+module.exports = {
+  locale: findLocale,
+  defaultTranslation: findDefaultTranslation,
+  translations: findTranslations,
+  // post_categories: findPostCategories,
+  post_categories: data => findPostAttributes(data, 'post_categories'),
+  post_authors: data => findPostAttributes(data, 'post_authors'),
+  id: findId,
+  pageData: data => omitSomeEleventySuppliedData(data)
+}
