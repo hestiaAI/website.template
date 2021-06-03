ce6967e034f6eab40aa9829c08c2bf8a3cce8660
Olivier Lange <olange@petit-atelier.ch>
2021-06-03 17:28:21 +0200

    Adds ‹newsletter-signup› form to homepage

--- src/components/README.md
+++ src/components/README.md
@@ -1,3 +1,54 @@
 # Web Components
 
 Web Components of our website.
+
+## Available components
+
+* `<article-breakout href="…"> …text… </article-breakout>`
+* `<contact-card img-src="portrait.jpg"> …text… </contact-card>`
+* `<newsletter-signup form-name="newsletter-signup" lang="en"></newsletter-signup>`
+* `<video-player src="…" img-src="…"> …text… </video-player>`
+
+## Usage
+
+Sample usage of the ‹article-breakout› component; applies to all our components:
+
+```javascript
+<article-breakout layout="box"
+  href="en/blog/editorial/first-post/"
+  img-src="illustration.jpg">
+  <span slot="caption">photo by …</span>
+  <h2>A first blog post</h2>
+  <p>An <em>attention</em>-<strong>grabbing</strong> lead.</p>
+  <button>Read more</button>
+</article-breakout>
+
+<script type="module">
+  import '/components/article-breakout.js';
+</script>
+```
+
+## Disambiguation
+
+By ‹Web Components›, in this project, we mean the standardized _front-end_ [Web Components](https://www.webcomponents.org/introduction), which incarnate as custom elements in web pages, registered and instanciated at _runtime_ by the browser. For instance:
+
+```html
+<sar-helper domain="dating-apps">
+Reclaim your <em>Dating App</em> data!
+</sar-helper>
+```
+
+As we are also using [Eleventy](https://www.11ty.dev), to statically generate our websites, the following might confuse you:
+
+* reusable fragments of content, such as _includes_ and _layouts_, might be understood as «web components», conceptually;
+* [Eleventy template shortcodes](https://www.11ty.dev/docs/shortcodes/), that is, parametrized _macros_ which generate code, are conceptually similar to «web components» too;
+* however both run at _build time_, inlining those fragments of reusable HTML5/CSS code.
+
+This issue gets coarser, when you consider that:
+
+1. Without a static site generator, you tend to develop a lot of ‹Web Components›; with a static site generator, some ‹Web Component› can be replaced with inlined HTML5/CSS code (you loose CSS encapsulation along the way, but other tools allow to solve the namespacing issues), so you tend to not use ‹Web Components› until late in the implementation; drawing the line between what goes in the «macros» of Eleventy and ‹Web Components› is more difficult, when using static site generators.
+2. Other static generators (such as [stencil.js](https://stenciljs.com)) allow produce ‹Web Components›, instead of inlining HTML5/CSS code; and for good reason, as they are aimed at producing _Design Systems_.
+
+We are aimed at producing a _Design System_ too; at therefore inclined to introduce ‹Web Components› rather early than late in the implementation. 
+
+Yet we are at the very beginning of our journey, so determining how to implement _reusable functionality_ might require further conversation — don't hesitate to reach out to [@olange](http://github.com/olange) with your questions.
---
+++ src/components/newsletter-signup.js
@@ -0,0 +1,194 @@
+import { LitElement, html, css } from 'lit-element';
+import { registerTranslateConfig, use, translate } from "lit-translate";
+
+const LAYOUT_NORMAL = "normal",
+      LAYOUT_COMPACT = "compact",
+      LAYOUT_DEFAULT = LAYOUT_NORMAL,
+      LAYOUT_ENUM = [LAYOUT_NORMAL, LAYOUT_COMPACT];
+
+const DEFAULT_TRANSLATIONS = {
+  "title": "Sign up to our newsletter",
+  "info": "We won't really spam you",
+  "first_name": "First name",
+  "last_name": "Last name",
+  "email": "Email address",
+  "submit_button": "Send"
+};
+
+registerTranslateConfig({
+  loader: async (lang) => {
+    try {
+      const result = await fetch(`/assets/i18n/newsletter-signup.json`);
+      const translations = await result.json();
+      return translations[lang] || DEFAULT_TRANSLATIONS;
+    } catch(error) {
+      return  DEFAULT_TRANSLATIONS;
+    }
+  }});
+
+/**
+ * ‹newsletter-signup› custom element, displaying a
+ * form to sign up for a newsletter.
+ *
+ * Submitted values will be sent to and stored into the
+ * Netlify Forms service.
+ *
+ * Note:
+ *
+ *   Netlify buildbots find the forms by parsing the
+ *   static HTML of the site when the build completes; as
+ *   this Web Component renders the form client-side, the
+ *   buildbots won't find the form.
+ *   To work around this, a hidden form needs to be added
+ *   to the static HTML, somewhere outside of this Web Component,
+ *   with the same form structure as the one rendered hereafter.
+ *
+ * See also:
+ *   Netlify Docs › Forms setup › JavaScript forms
+ *   https://docs.netlify.com/forms/setup/#work-with-javascript-rendered-forms
+ *
+ * Attributes:
+ *
+ *   lang: String
+ *     Language of the page on which the component
+ *     is displayed; will be placed in a hidden field,
+ *     to be stored with the other fields of the form.
+ *
+ *   form-name: String
+ *     Name of the Netlify Form.
+ *
+ *   layout (optional): "normal" (default) | "compact"
+ *     Determines how to display the form to the user:
+ *     `normal` displays title, info, fields and their labels;
+ *     `compact` display title and fields only, with
+ *     placeholders inside fields instead of labels.
+ *
+ * Usage:
+ *
+ *     <form name="newsletter-signup" method="POST" data-netlify="true">
+ *         <input type="hidden" name="first-name">
+ *         <input type="hidden" name="last-name">
+ *         <input type="hidden" name="email">
+ *     </form>
+ *
+ *     <newsletter-signup
+ *         lang="en" layout="compact"
+ *         form-name="newsletter-signup">
+ *     </newsletter-signup>
+ *
+ */
+export class NewsletterSignup extends LitElement {
+
+  static get styles() {
+    return css`
+        @import '/assets/styles/vendor/normalize.css';
+        @import '/assets/styles/base/typography.css';
+        @import '/assets/styles/base/spacing.css';
+        @import '/assets/styles/elements/buttons.css';
+        @import '/assets/styles/elements/forms.css';
+
+        :host {
+          display: block; }
+
+        h3, p { margin: 0; }
+
+        p { margin-bottom: 0.5rem; }
+
+        button { margin-top: 0.5rem; }
+
+        .signup-fields {
+          display: flex; }
+
+        .signup-field {
+          flex: 1;
+          min-width: 48%;
+          padding-left: 1rem;
+          padding-bottom: 0.5rem; }
+
+        .signup-field:first-child {
+          padding-left: 0; }
+
+        .signup-field input {
+          display: inline-block;
+          width: 100%; }
+    `;
+  }
+
+  static isValidLayout( value) {
+    return typeof value === "string"
+      && LAYOUT_ENUM.includes( value);
+  }
+
+  static get properties() {
+    return {
+      lang: { type: String },
+      netlifyFormName: { type: String, attribute: "form-name" },
+      layout: {
+        type: String,
+        converter: {
+          fromAttribute: function normalizeLayoutProp( value, type) {
+            return NewsletterSignup.isValidLayout( value)
+              ? String( value) : LAYOUT_DEFAULT;
+          }
+        }
+      }
+    }
+  }
+
+  constructor() {
+    super();
+  }
+
+  firstUpdated() {
+    // Load the default language
+    use(this.lang);
+  }
+
+  render() {
+    return html`
+      <h3>${translate("title")}</h3>
+      ${this.layout === LAYOUT_COMPACT || !translate("info")
+        ? '' : html`<p>${translate("info")}</p>` }
+      <form class="signup" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="${this.netlifyFormName}" />
+        <input type="hidden" name="lang" value="${this.lang}" />
+        <div class="signup-fields">
+          <div class="signup-field">
+            ${this.layout === LAYOUT_COMPACT ? '' : html`
+              <label for="signup-first-name">
+                ${translate("first_name")}
+              </label>`}
+            <input type="text" name="first-name"
+                   id="signup-first-name"
+                   placeholder="${translate("first_name")}">
+          </div>
+          <div class="signup-field">
+            ${this.layout === LAYOUT_COMPACT ? '' : html`
+              <label for="signup-last-name">
+                ${translate("last_name")}
+              </label>`}
+            <input type="text" name="last-name"
+                   id="signup-last-name"
+                   placeholder="${translate("last_name")}">
+          </div>
+        </div>
+        <div class="signup-fields">
+          <div class="signup-field">
+            ${this.layout === LAYOUT_COMPACT ? '' : html`
+              <label for="signup-email">
+                  ${translate("email")}
+              </label>`}
+            <input type="email" name="email"
+                   id="signup-email"
+                   placeholder="${translate("email")}">
+          </div>
+        </div>
+        <div>
+          <button type="submit">${translate("submit_button")}</button>
+        </div>
+      </form>
+    `;
+  }
+}
+
+customElements.define('newsletter-signup', NewsletterSignup);
